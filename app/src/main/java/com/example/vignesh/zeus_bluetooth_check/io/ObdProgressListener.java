78ec9b4390f7197e80f82dec17884a25899e99ab
Vignesh B <vigneshbv02@gmail.com>
2018-03-07 01:47:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vignesh/zeus_bluetooth_check/io/ObdProgressListener.java
@@ -0,0 +1,10 @@
+package com.example.vignesh.zeus_bluetooth_check.io;
+
+/**
+ * TODO put description
+ */
+public interface ObdProgressListener {
+
+    void stateUpdate(final ObdCommandJob job);
+
+}
